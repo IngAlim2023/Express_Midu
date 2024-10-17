94e1823f2b7f8e0c66659dfc718e63c4ffc9471e
IngAlim2023 <enrique.porras@udea.edu.co>
2024-10-17 09:14:09 -0500

    Servidor creado con urls

--- http.js
+++ http.js
@@ -1,11 +1,26 @@
 const http = require('node:http');  // protocolo HTTP
 const desiredPort = process.env.PORT ?? 1234
 
-const server = http.createServer((req, res)=>{
-    console.log('request received')
-    res.end('Holas')
-})
+
+const processRequest = (req, res) =>{
+
+    res.setHeader('Content-Type', 'text/html; charset=utf-8')
+
+    if (req.url === "/"){
+        res.statusCode = 200
+        res.end('<h1>Hello, World!, Hola compañia</h1>');
+    } else if (req.url === "/contacto"){
+        res.statusCode = 200
+        res.end('<h1>Hola contacto</h1>');
+    } else{
+        res.statusCode = 404
+        res.end('<h1>404 Not Found</h1>');
+    }
+    
+};
+
+const server = http.createServer(processRequest);
 
 server.listen(desiredPort, ()=>{
-    console.log(`server listening on port ${desiredPort}`)
+    console.log(`server listening on port http://localhost:${desiredPort}`)
 })
