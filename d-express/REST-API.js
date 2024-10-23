0aff2e05be2600fc0dcef7131c72b116913145d2
IngAlim2023 <enrique.porras@udea.edu.co>
2024-10-23 08:04:32 -0500

    rutas static, cru sin delete de producto

--- d-express/REST-API.js
+++ d-express/REST-API.js
@@ -4,6 +4,15 @@ const express = require("express");
 //Creamos la app que va a contener todo lo relacionado con Express:
 const app = express();
 
+//Generamos un arreglo para que se pueda mostrar en la solicitud GET:
+const produtcs = [
+    {
+        id:1,
+        name:"Laptop Mac",
+        price: 1000
+    }
+]
+
 //Generamos el puerto por donde va a estar disponible nuestra API:
 //Si no hay un puerto definido en el entorno, se usa el 3500 por default
 const PORT = process.env.PORT ?? 3500;
@@ -16,18 +25,50 @@ app.use((req, res, next)=>{
     next();
 })
 
+//Usamos el middleware para que se pueda leer el body de la solicitud
+
+app.use(express.json());
+
+// Configuramos las rutas de los archivos estáticos:
+app.use('/static', express.static('assets'))
+
 // Generamos las rutas o endpoints para nuestra API:
 
 app.get('/', (req, res)=>{
-    res.send("<h1>Corriste el servidor correctamente, Congratulations!!!!</h1>")
+    res.sendFile("./vista.html",
+        root = {
+            root: __dirname
+        }
+    )
+})
+
+app.get('/perro', (req, res)=>{
+    res.sendFile("./index.html",
+        root = {
+            root: __dirname
+        }
+    )
+})
+
+app.get('/bibliografia', (req,res)=>{
+    res.sendFile("./bibliografia.html",
+        root = {
+            root: __dirname
+
+        }
+)
 })
 
 app.get('/productos', (req, res)=>{
-    res.send("<h1>Bienvenido a productos GET!!!!</h1>")
+    res.json(produtcs);
 })
 
 app.post('/productos', (req, res)=>{
-    res.send("<h1>POST productos!!!!</h1>")
+    console.log(req.body);
+    const  producto = {...req.body, id:  produtcs.length + 1};
+    produtcs.push(producto);
+    console.log(produtcs);
+    res.json(produtcs);
 })
 
 app.put('/productos', (req, res)=>{
@@ -39,7 +80,16 @@ app.delete('/productos', (req, res)=>{
 })
 
 app.get('/productos/:id', (req, res)=>{
-    res.send("<h1>Obtener un producto!!!!</h1>")
+    const  id = req.params.id;
+    const  productoEncontrado = produtcs.find((p)=> p.id === parseInt(id));
+
+    console.log(productoEncontrado);
+    if (productoEncontrado){
+        res.json(productoEncontrado);
+    }else{
+        res.status(404).send("<h1>Producto no encontrado</h1>")
+    }
+    
 })
 
 app.use((req, res)=>{
